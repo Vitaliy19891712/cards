caf9b37540b7976075d95906b5ab9f97810b8a8f
Vitaliy19891712 <stopkaiko@mail.ru>
2023-08-01 20:14:21 +0300

    add dropdownmenu component

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
+import { Dropdownmenu } from './components/ui/dropDownMenu'
 import { SliderCommon } from './components/ui/slider'
 
 export function App() {
   return (
-    <SliderCommon
-      currentMax={100}
-      currentMin={0}
-      max={100}
-      min={0}
-      onChange={value => {
-        console.log(value)
-      }}
-    />
+    <div className="qwe">
+      <Dropdownmenu  modal={true} onChange={() => {}} items={[326666, 35, 645]}>
+        <div>123</div>
+      </Dropdownmenu>
+    </div>
   )
 }
---
+++ src/components/ui/dropDownMenu/dropdownmenu.tsx
@@ -0,0 +1,29 @@
+import { ReactNode, useState } from 'react'
+import s from './dropdownmenu.module.scss'
+import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
+type IProps = {
+  onChange: (open: boolean) => void
+  modal?: boolean
+  children: ReactNode
+  items: Array<ReactNode>
+  trigger?: ReactNode
+}
+
+export const Dropdownmenu: React.FC<IProps> = ({ modal, items, trigger }) => {
+  const [open, setOpen] = useState(false)
+
+  return (
+    <DropdownMenu.Root defaultOpen={false} open={open} onOpenChange={setOpen} modal={modal}>
+      <DropdownMenu.Trigger asChild>{trigger}</DropdownMenu.Trigger>
+
+      <DropdownMenu.Content className={s.content} loop={true} sideOffset={4}>
+        {items.map(i => {
+          return <DropdownMenu.Item className={s.item}>{i}</DropdownMenu.Item>
+        })}
+
+        <DropdownMenu.Separator className={s.separator} />
+        <DropdownMenu.Arrow className={s.arrow} width={15} height={8} />
+      </DropdownMenu.Content>
+    </DropdownMenu.Root>
+  )
+}
